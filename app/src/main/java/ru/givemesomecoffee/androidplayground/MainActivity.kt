c75b193f06f68efe3de2bf8f06b257c6c01903c7
givemesomecoffee <givemesomecoffee@yandex.ru>
2021-09-27 11:03:09 +0300

    robo test

--- app/src/main/java/ru/givemesomecoffee/androidplayground/MainActivity.kt
+++ app/src/main/java/ru/givemesomecoffee/androidplayground/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 import ru.givemesomecoffee.androidplayground.data.network.BooksApi
 
-
+//robo commit1
+//1
+//wsd
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
